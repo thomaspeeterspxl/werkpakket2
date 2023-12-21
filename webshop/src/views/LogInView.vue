fae41d9668ce35b42f2b6098e245a21b6a843bac
thomaspeeterspxl <thomas.peeters3@student.pxl.be>
2023-12-21 19:13:52 +0100

    save

--- webshop/src/views/LogInView.vue
+++ webshop/src/views/LogInView.vue
@@ -4,10 +4,10 @@
     <h1>Login</h1>
     <form id="login-form">
       <label class="form-label" for="email">E-mailadres:</label>
-      <input  type="email" id="email" name="email" required>
+      <input  type="email" id="email" name="email" placeholder="e-mail" required>
 
       <label class="form-label" for="password">Wachtwoord:</label>
-      <input  type="password" id="password" name="password" required>
+      <input  type="password" id="password" name="password" placeholder="wachtwoord" required>
 
       <button type="submit" class="button"><router-link to="/">Inloggen</router-link></button>
     </form>
